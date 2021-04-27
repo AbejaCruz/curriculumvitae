28e47908f760f4fd07a950016a47617fb3a01122
Diego Alberto Cruz <dacruzt@outlook.com>
2021-04-27 18:42:47 -0500

    Create data.js

---
+++ data.js
@@ -0,0 +1,91 @@
+{
+    "data": {
+        "avatar": "https://s3.amazonaws.com/chewiekie/img/photo.jpg",
+        "name": "Diego A Cruz Triana",
+        "profession": "Web Developer",
+        "bio": "Ingeniero de sistemas, Amante de los videjuegos y del constante aprendizaje.",
+        "address": "Colombia, Colombia.",
+        "social": {
+            "0": {
+                "name": "facebook",
+                "url": "https://www.facebook.com/diegabeja"
+            },
+            "1": {
+                "name": "twitter",
+                "url": "https://twitter.com/AbejaCruzDev"
+            },
+            "2": {
+                "name": "github",
+                "url": "https://github.com/AbejaCruz"
+            },
+            "3": {
+                "name": "linkedin",
+                "url": "https://www.linkedin.com/in/abejacruz/"
+            }
+        },
+        "experience": {
+            "0": {
+                "jobTitle": "Desarrollador VR/AR",
+                "company": "Mudi S.A.S",
+                "startDate": "Mayo 2019",
+                "endDate": "Febrero 2021",
+                "jobDescription": "Desarrollo full stack de Aplicaciones de realidad amentada para la web, aplicaciones de VR para dispositivos y landing pages."
+            },
+            "1": {
+                "jobTitle": "Frontend",
+                "company": "Tresciclaje",
+                "startDate": "Marzo 2017",
+                "endDate": "Diciembre 2018",
+                "jobDescription": "Desarrollo del Frontend de la paltaforma Tresciclaje."
+            }
+        },
+        "education": {
+            "0": {
+                "degree": "Tecnólogo en Producción Multimedia",
+                "institution": "SENA",
+                "startDate": "Enero 2016",
+                "endDate": "Diciembre 2018",
+                "description": ""
+            },
+            "1": {
+                "degree": "Ingeniería de Sistemas",
+                "institution": "Universidad Distrital Francisco José de Caldas",
+                "startDate": "Enero 2010",
+                "endDate": "Mayo 2017",
+                "description": ""
+            }
+        },
+        "certificate": {
+            "0": {
+                "name": "Escuela de Javascript",
+                "institution": "Platzi",
+                "date": "2019",
+                "description": "La Escuela de JavaScript es el mejor esfuerzo de blended education, educación online y presencial para especializarte en desarrollo Full Stack con JavaScript. "
+            },
+            "1": {
+                "name": "FrontEnd Developer",
+                "institution": "Platzi",
+                "date": "Enero 2019",
+                "description": "Carrera de FrontEnd de Platzi"
+            }
+        },
+        "skills": {
+            "0": {
+                "name": "HTML5",
+                "percentage": "90%"
+            },
+            "1": {
+                "name": "CSS",
+                "percentage": "85%"
+            },
+            "2": {
+                "name": "JavaScript",
+                "percentage": "80%"
+            },
+            "3": {
+                "name": "React",
+                "percentage": "60%"
+            }
+        }
+    }
+}
